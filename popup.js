0dc640b34489ef56b300303a12c45396a3a61536
jnvillalba <joaquinvillalba8@gmail.com>
2023-05-31 14:38:59 -0300

    switch

--- popup.js
+++ popup.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('toggleButton');
-  
-    // Enviar un mensaje al script de contenido para activar o desactivar la ocultación de la barra/indicador de tiempo y la duración del video
-    toggleButton.addEventListener('click', function () {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleHideTimeBar' });
-      });
+  const toggleButton = document.getElementById('toggleSwitch');
+
+  // Enviar un mensaje al script de contenido para activar o desactivar la ocultación de la barra/indicador de tiempo y la duración del video
+  toggleButton.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleHideTimeBar' });
     });
   });
-  
+});
