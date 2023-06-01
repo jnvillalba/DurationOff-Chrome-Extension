8dd5714bdfe72703ba6bb527228158aeb2902a23
jnvillalba <joaquinvillalba8@gmail.com>
2023-06-01 16:27:44 -0300

    storage

--- popup.js
+++ popup.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
   const toggleButton = document.getElementById('toggleSwitch');
 
+  // Obtener el estado guardado del switch
+  chrome.storage.local.get('switchState', function (result) {
+    if (result.switchState === 'enabled') {
+      toggleButton.checked = true;
+    } else {
+      toggleButton.checked = false;
+    }
+  });
+
   // Enviar un mensaje al script de contenido para activar o desactivar la ocultación de la barra/indicador de tiempo y la duración del video
   toggleButton.addEventListener('click', function () {
+    const switchState = toggleButton.checked ? 'enabled' : 'disabled';
+
+    // Guardar el estado del switch
+    chrome.storage.local.set({ switchState });
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleHideTimeBar' });
     });
   });
 });
+
 
 //TODO
 //que recuerde la eleccion local y en todas las pestañas
